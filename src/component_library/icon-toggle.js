ecc171f3ddd9bc29bf9fdfb25845065a7ea7be20
Mark Armendariz <armendariz.mark@yahoo.com>
2018-12-02 12:32:34 -0800

    Added setup for basic layout

--- src/component_library/icon-toggle.js
+++ src/component_library/icon-toggle.js
@@ -1,8 +1,8 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
 import '@polymer/iron-icon/iron-icon';
+import '@polymer/iron-icons/iron-icons';
 
 export class IconToggle extends PolymerElement{
-
 
     static get template(){
         return html`
@@ -10,15 +10,52 @@ export class IconToggle extends PolymerElement{
         :host {
           display: block;
         }
+        iron-icon{
+            fill: var(--icon-toggle-color, rgba(0,0,0,0));
+            stroke: var(--icon-toggle-outline-color, currentcolor);
+        }
+        :host([pressed]) iron-icon{
+            fill: var(--icon-toggle-pressed-color, currentcolor);
+        }
+
         </style>
 
         <h1>dfdf</h1>
+        
+        <iron-icon icon="{{toggleIcon}}"></iron-icon>
+
 
 
 
         `;
     }
 
+    constructor(){
+        super();
+        this.addEventListener('click', this.toggle.bind(this));
+    }
+
+    static get properties(){
+        return{
+
+            toggleIcon: {
+                type: String
+            },
+            pressed:{
+                type: Boolean,
+                value: false,
+                notify: true,
+                reflectToAttribute: true
+            }
+            
+        };
+    }
+
+    toggle() {
+        this.pressed = !this.pressed;
+    }
 
 
 }
+
+window.customElements.define('icon-toggle', IconToggle);
--- src/showcase-application/showcase-application.js
+++ src/showcase-application/showcase-application.js
@@ -1,7 +1,9 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
 import {ScMenuBar} from '../component_library/sc-menu-bar/sc-menu-bar.js';
 import {IconToggle} from '../component_library/icon-toggle.js';
-
+import '@polymer/app-route/app-location';
+import '@polymer/app-route/app-route';
+import '@polymer/iron-pages/iron-pages';
 
 /**
  * @customElement
@@ -15,13 +17,24 @@ class ShowcaseApplication extends PolymerElement {
       <style>
         :host {
           display: block;
+          --icon-toggle-outline-color: blue;
         }
       </style>
 
       <sc-menu-bar on-reset-clicked></sc-menu-bar>
 
       <h2>Hello [[prop1]]!</h2>
-      <icon-toggle></icon-toggle>
+      <icon-toggle toggle-icon="star" pressed></icon-toggle>
+
+      <iron-pages>
+        <div id="EducationPage"></div>
+        <div id="DirectExperiencePage"></div>
+        <div id="SpecialProjectsPage"></div>
+        <div id="Additional Experience Page"></div>
+        <div id="SkillsPage"></div>
+      </iron-pages>
+      
+      
 
     `;
   }
