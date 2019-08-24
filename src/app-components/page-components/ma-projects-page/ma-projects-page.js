0c834de0c951ea541bb469cacc4b0b55a5396d16
Mark Armendariz <armendariz.mark@yahoo.om>
2019-08-23 21:02:54 -0700

    Changes to projects page to include link to projects.themarkarmendariz.com

--- src/app-components/page-components/ma-projects-page/ma-projects-page.js
+++ src/app-components/page-components/ma-projects-page/ma-projects-page.js
@@ -28,17 +28,27 @@ export class MaProjectsPage extends PolymerElement {
           @apply --ma-resume-page-style;
         }
         #comingSoonCard{
-          margin-top: 40px;
+          margin-top: 100px;
         }
         #comingSoonCard div[slot="content"]{
           background-color : var(--ma-main-theme-color);
+          height: 100px;
         }
-        #comingSoonMsg, a{
+        #comingSoonCard div[slot="content"]:hover{
+          background-color : white;
+          color: black;
+        }
+        #comingSoonMsg{
           text-align: center;
           color: white;
           text-decoration: none;
           font-size: 20pt;
-          height: 50px;
+          display: block;
+          line-height: 100px;
+          font-weight: bold;
+        }
+        #comingSoonMsg:hover{
+          color: black;
         }
         @media (min-width: 320px) and (max-width: 480px) {
           #comingSoonCard{
@@ -56,7 +66,7 @@ export class MaProjectsPage extends PolymerElement {
 
         <ma-card id="comingSoonCard">
           <div slot="content">
-            <p id="comingSoonMsg"><a href="{{url}}" target="_blank">Enter Projects Site</a></p>
+            <a id="comingSoonMsg" href="{{url}}" target="">Enter Projects Website</a>
           </div>
         </ma-card>
 
